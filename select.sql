eb154482f106b121f23a94619d698bb5cdb8f4f4
Ayesha Suleman <123162719+Ayesha999-hub@users.noreply.github.com>
2025-03-06 14:51:35 +0500

    Add files via upload

---
+++ select.sql
@@ -0,0 +1,9 @@
+/*Question
+Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.*/
+
+/*Approach
+retrieved the column name from city table where country code is equal to'USA'and popualtion is greater than 120000*/ 
+
+select name from city 
+where countrycode = 'USA'
+and population > 120000;
