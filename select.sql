61c6814c07d31926ca9c461e0fd51d06eff237ba
Ayesha Suleman <123162719+Ayesha999-hub@users.noreply.github.com>
2025-03-07 17:51:18 +0500

    Update select.sql

--- select.sql
+++ select.sql
@@ -59,5 +59,63 @@ Approach : computed the difference between the acttual no of cities and distinct
 select count(city) - count(distinct(city))
 from station;
 
+/*
+Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
+
+Approach: fetched distinct city names whose name starts and ends wih vowels using left and right function*/
+select distinct city from station
+where left(city ,1 ) in ('a','e','i','o','u') and
+right(city ,1) in ('a','e','i','o','u');
+
+/*Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+Approach: queried the names of cities whose starting letter is not vowel by usinf not regexp function*/
+select distinct city 
+from station
+where city not regexp '^[aeiou]';
+
+/*Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates
+Approach: feched the city names whose ending letter is not vowel by using not regexp function*/
+
+select distinct city 
+from station
+where city not regexp '[aeiou]$';
+
+/*Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+
+Approach: fetched the name of cities whose name nor start neiher end with a vowel by using not regexp function*/
+
+select distinct city 
+from station
+where city not regexp '^[aeiou]' or city not regexp '[aeiou]$';
+
+/*Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+
+Approach:retrieved the city names whose name donot start and donot end with a vowel by using and with not regexp function*/
+
+select distinct city 
+from station
+where city not regexp '^[aeiou]' and city not regexp '[aeiou]$';
 
 
+/*Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+
+Approach: retrieved the names of students whose marks are greter than 75 and ordered by last character in asc and th
+e the one with same last three characters with id asc*/
+
+select name from students
+where marks > 75
+order by right(name , 3 ) asc , ID asc   ;
+
+/*Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+Approach : using order by to  retrieve the employee names alphabetically */
+
+select  name from employee
+order by name asc;
+
+/*Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater than $2000  per month who have been employees for less than 10  months. Sort your result by ascending employee_id.
+Approach: retrived the names of employees whose slaryy is gereater than $2000 and employeess experience less tthan 10 months by using and condition while ordering by asc */
+
+select name from employee 
+where salary > 2000 and months < 10
+order by employee_id;
+ 
